e8fb588bba8b272e9bdb716f441a0d8a8483a6d9
messaoudinour <nour.messaoudi@esprit.tn>
2026-02-16 20:09:51 +0100

    Init

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM eclipse-temurin:17-jdk-jammy
+
+WORKDIR /app
+
+COPY target/timesheet-devops-1.0.jar app.jar
+
+EXPOSE 8082
+
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -0,0 +1,84 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "t_user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String role;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Temporal(TemporalType.DATE)
+    private Date dateNaissance;
+
+    // ---------------- Constructors ----------------
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String role, Date dateNaissance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+        this.dateNaissance = dateNaissance;
+    }
+
+    // ---------------- Getters & Setters ----------------
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+}
