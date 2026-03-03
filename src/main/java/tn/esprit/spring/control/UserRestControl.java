5621b6546f55e44b7a7e8f97c36d3e747570bdee
messaoudinour <nour.messaoudi@esprit.tn>
2026-03-03 12:11:19 +0100

    Fix Sonar issues - constructor injection + clean code

--- src/main/java/tn/esprit/spring/control/UserRestControl.java
+++ src/main/java/tn/esprit/spring/control/UserRestControl.java
@@ -2,57 +2,33 @@ package tn.esprit.spring.control;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import tn.esprit.spring.entities.User;
 import tn.esprit.spring.services.IUserService;
 
-// userRestControl
-@RestController // = @Controller + @ResponseBody 
+@RestController
 @RequestMapping("/user")
-public class UserRestControl {
+public class UserRestController {
 
-	@Autowired 
-	IUserService userService; 
+    private final IUserService userService;
 
-	
-	// URL : http://localhost:????/????/????/retrieve-all-users
-	@GetMapping("/retrieve-all-users")
-	public List<User> retrieveAllUsers() {
-		return userService.retrieveAllUsers();
-		//return list;
-	}
- 
-	// http://localhost:????/timesheet-devops/retrieve-user/{user-id}
-	@GetMapping("/retrieve-user/{user-id}")
-	public User retrieveUser(@PathVariable("user-id") String userId) {
-		return userService.retrieveUser(userId);
-	}
-	
-	 
+    public UserRestController(IUserService userService) {
+        this.userService = userService;
+    }
 
-	// Ajouter User : http://localhost:????/timesheet-devops/add-user 
-	@PostMapping("/add-user")
-	public User addUser(@RequestBody User u) {
-		User user = userService.addUser(u); 
-		return user;
-	}
+    @GetMapping("/retrieve-all-users")
+    public List<User> retrieveAllUsers() {
+        return userService.retrieveAllUsers();
+    }
 
-	
-	// Supprimer User : 
-	// http://localhost:????/timesheet-devops/remove-user/{user-id}
-	@DeleteMapping("/remove-user/{user-id}") 
-	public void removeUser(@PathVariable("user-id") String userId) { 
-		userService.deleteUser(userId);
-	} 
+    @GetMapping("/retrieve-user/{user-id}")
+    public User retrieveUser(@PathVariable("user-id") String userId) {
+        return userService.retrieveUser(userId);
+    }
 
-	// Modifier User 
-	// http://localhost:????/timesheet-devops/modify-user 
-	@PutMapping("/modify-user") 
-	public User updateUser(@RequestBody User user) {
-		return userService.updateUser(user);
-	}
-	 
-} 
- 
+    @PostMapping("/add-user")
+    public User addUser(@RequestBody User u) {
+        return userService.addUser(u);
+    }
+}
